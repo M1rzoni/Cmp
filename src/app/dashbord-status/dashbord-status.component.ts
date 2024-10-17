0f4b007d657f7637d08a84c600566aba0580def7
Mirza <mirza.sabanovic@ipi-akademija.ba>
2024-10-17 18:22:12 +0200

    Separting code  with components.

---
+++ src/app/dashbord-status/dashbord-status.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbord-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashbord-status.component.html',
+  styleUrl: './dashbord-status.component.css',
+})
+export class DashbordStatusComponent {
+  currentStatus = 'online';
+}
---
+++ src/app/dashbord-support/dashbord-support.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbord-support',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashbord-support.component.html',
+  styleUrl: './dashbord-support.component.css'
+})
+export class DashbordSupportComponent {
+
+}
